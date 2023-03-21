88a7e35d3c42be029443fb049c5447ce0534585e
Daniela Weiss <daniela.weiss@mail.de>
2023-03-21 11:40:50 +0100

    separate the creation of the file with the token frequencies and the lexical feature extraction into to 2 files

---
+++ frequencies_table.py
@@ -0,0 +1,48 @@
+# Create a file with token frequencies
+# based on this DeReWo corpus
+# from https://www.ids-mannheim.de/digspra/kl/projekte/methoden/derewo/
+import json
+
+
+def get_frequency_table_from_json_file(json_file_path):
+    """Returns frequency table a dict with token as keys and freqs as values.
+    :param json_file_path: (str)
+    :return: dict """
+    with open(json_file_path, "r", encoding="utf-8") as file:
+        frequency_tabel = json.load(file)
+
+    return frequency_tabel
+
+
+def get_token_frequencies_from_corpus(corpus_path):
+    """ Returns the frequencies table , which is a dict with token as keys
+    and frequencies as values for the DeDrKo corpus.
+        ### the corpus looks like this:
+            ist	sein	VAFIN	64833211
+            im	im	APPRART	62591891
+            für	für	APPR	56990511
+            des	die	ART	56555049.7224575
+    :param corpus_path: str
+    :return: dict """
+    frequency_table = dict()
+    with open(corpus_path, "r", encoding="utf-8") as file:
+        for line in file:
+            act_line = line.split()
+            # the DeReWo corpus is case sensitive so here lowering all tokens
+            frequency_table[act_line[0].lower()] = float(act_line[-1])
+
+    return frequency_table
+
+
+def create_file_with_token_frequencies():
+    """Creates a json file in data with the frequency table a
+    dict with token as keys and freqs as values.
+    :param json_file_path: (str) """
+    corpus_path = "data/DeReWo/DeReKo-2014-II-MainArchive-STT.100000.freq"
+    data = get_token_frequencies_from_corpus(corpus_path)
+    with open('data/token_freq_table.json', 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    create_file_with_token_frequencies()
--- lexical_features.py
+++ lexical_features.py
@@ -1,7 +1,8 @@
 # type-token-ratio and lexical-complexity-score
 import spacy
 import numpy as np
-import json
+
+from frequencies_table import  get_frequency_table_from_json_file
 
 
 def calculate_ttr(doc):
@@ -51,46 +52,6 @@ def calculate_lexical_complexity_score(doc, tokens_freq):
     return third_quartile
 
 
-def get_token_frequencies_from_corpus(corpus_path):
-    """ Returns the frequencies table , which is a dict with token as keys
-    and frequencies as values for the DeDrKo corpus.
-        ### the corpus looks like this:
-            ist	sein	VAFIN	64833211
-            im	im	APPRART	62591891
-            für	für	APPR	56990511
-            des	die	ART	56555049.7224575
-    :param corpus_path: str
-    :return: dict """
-    frequency_table = dict()
-    with open(corpus_path, "r", encoding="utf-8") as file:
-        for line in file:
-            act_line = line.split()
-            # the DeReWo corpus is case sensitive so here lowering all tokens
-            frequency_table[act_line[0].lower()] = float(act_line[-1])
-
-    return frequency_table
-
-
-def get_frequency_table_from_json_file(json_file_path):
-    """Returns frequency table a dict with token as keys and freqs as values.
-    :param json_file_path: (str)
-    :return: dict """
-    with open(json_file_path, "r", encoding="utf-8") as file:
-        frequency_tabel = json.load(file)
-
-    return frequency_tabel
-
-
-def create_file_with_word_frequencies():
-    """Creates a json file in data with the frequency table a dict with token as keys and freqs as values.
-        # from https://www.ids-mannheim.de/digspra/kl/projekte/methoden/derewo/
-    :param json_file_path: (str) """
-    corpus_path = "data/DeReWo/DeReKo-2014-II-MainArchive-STT.100000.freq"
-    data = get_token_frequencies_from_corpus(corpus_path)
-    with open('data/token_freq_table.json', 'w') as f:
-        json.dump(data, f)
-
-
 def demo():
     nlp = spacy.load("de_core_news_sm")
     text = "Die Bananen sind reif. Ich esse gerne eine Banane, weil sie so schön gelb ist. " \
@@ -130,6 +91,5 @@ def demo():
 
 
 if __name__ == "__main__":
-    # create_file_with_word_frequencies()
     demo()
 
