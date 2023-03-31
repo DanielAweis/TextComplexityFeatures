d21258bc23b754e8c270966222271d7d9804094f
Daniela Weiss <daniela.weiss@mail.de>
2023-03-31 09:20:00 +0200

    add nummeric label to matrix

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+import csv
 
 
 def get_data_from_json_file(json_file_path):
@@ -14,3 +15,35 @@ def get_data_from_json_file(json_file_path):
 def save_to_json_file(data, file_path):
     with open(file_path, "w", encoding="utf-8") as f:
         json.dump(data, f)
+
+
+def add_nummeric_label(vectores_file_path):
+    # "data/feature_vectors_L.csv"
+    data = []
+    with open(vectores_file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            new_line = []
+            if line.startswith("#"):
+                new_array = ["#label"]
+                new_array.extend(line.split(","))
+                line = new_array
+                data.append(new_array)
+            elif line.split(",")[0].startswith("miniklexi_"):
+                new_array = ["0.0"]
+                new_array.extend(line.split(","))
+                data.append(new_array)
+            elif line.split(",")[0].startswith("klexikon_"):
+                new_array = ["0.5"]
+                new_array.extend(line.split(","))
+                data.append(new_array)
+            elif line.split(",")[0].startswith("wiki_"):
+                new_array = ["1.0"]
+                new_array.extend(line.split(","))
+                data.append(new_array)
+
+    header = data[0]
+    with open("data/feature_df.csv", "w", encoding="utf-8") as csv_ofile:
+        writer = csv.writer(csv_ofile, delimiter=',')
+        writer.writerow(i for i in header)
+        for line in data[1:]:
+            writer.writerow(line)
