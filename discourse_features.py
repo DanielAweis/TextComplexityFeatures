1581c6279a99b92acfde321ea835fd1d2f8fb858
Daniela Weiss <daniela.weiss@mail.de>
2023-04-25 16:34:11 +0200

    bugfix

--- discourse_features.py
+++ discourse_features.py
@@ -80,12 +80,10 @@ def get_count_for_discourse_marker_senses(doc):
     dm_from_doc = find_discourse_markers(doc)
     # init empty vec
     counts_vec = list(repeat(0, len(all_disc_marker_senses)))
-    print(all_disc_marker_senses)
     for i in range(len(counts_vec)):
         for dm in dm_from_doc:
             if dm[1] == all_disc_marker_senses[i]:
-                increate_count = counts_vec[i] + 1
-                counts_vec.insert(i+1, increate_count)
+                counts_vec[i] = counts_vec[i] + 1
 
     return counts_vec
 
@@ -104,8 +102,8 @@ def get_discourse_features_for_all_docs(list_of_dicts, nlp):
     results = dict()
     for elem in list_of_dicts:
         doc = nlp(elem["text"])
-        # vec = get_count_for_discourse_marker_senses(doc)
-        vec = calculate_all_features(doc)
+        vec = get_count_for_discourse_marker_senses(doc)
+        #vec = calculate_all_features(doc)
         results[elem["id"]] = vec
     return results
 
@@ -127,7 +125,6 @@ def demo():
     print("Vector with counts of used discourse markers in doc:")
     print(get_count_for_discourse_marker_senses(doc))
 
-
     # output
     # Average count per sentence...
     # ... of pronouns: 1.6666666666666667
@@ -135,50 +132,6 @@ def demo():
     # ... of discourse markers: 0.5
     # Vector with counts of used discourse markers in doc:
     # [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-
-def main():
-    # TODO: integrate this in main script!
-    miniklexi_file_path = "data/miniklexi_corpus.txt"
-    klexikon_file_path = "data/klexi_corpus.txt"
-    wiki_file_path = "data/wiki_corpus.txt"
-
-    json_miniklexi = get_data_from_json_file(miniklexi_file_path)
-    json_klexikon = get_data_from_json_file(klexikon_file_path)
-    json_wiki = get_data_from_json_file(wiki_file_path)
-
-    miniklexi = json_miniklexi["einfache"]  # label 0.0
-    klexikon = json_klexikon["klexikon"]  # label 0.5
-    wiki = json_wiki["wiki"]  # label 1.0
-
-    nlp = spacy.load("de_core_news_md")
-    result_small_miniklexi = get_discourse_features_for_all_docs(miniklexi, nlp)
-    result_small_klexikon = get_discourse_features_for_all_docs(klexikon, nlp)
-    result_small_wiki = get_discourse_features_for_all_docs(wiki, nlp)
-
-    # save results in one csv file
-    header = ['#id']
-    header_features = list(feature_to_index.keys())
-    header.extend(header_features)
-
-    with open("data/discourse_feature_vectors.csv", "w", encoding="utf-8") as csv_ofile:
-        writer = csv.writer(csv_ofile, delimiter=',')
-        writer.writerow(i for i in header)
-        for key, value in sorted(result_small_miniklexi.items()):
-            line = [f"miniklexi_{key}"]
-            vecs = [round(i, 6) for i in value]
-            line.extend(vecs)
-            writer.writerow(line)
-        for key, value in sorted(result_small_klexikon.items()):
-            line = [f"klexikon_{key}"]
-            vecs = [round(i, 6) for i in value]
-            line.extend(vecs)
-            writer.writerow(line)
-        for key, value in sorted(result_small_wiki.items()):
-            line = [f"wiki_{key}"]
-            vecs = [round(i, 6) for i in value]
-            line.extend(vecs)
-            writer.writerow(line)
 
 
 def discourse_marker_senses():
@@ -226,5 +179,4 @@ def discourse_marker_senses():
 
 if __name__ == "__main__":
     # demo()
-    # main()
     discourse_marker_senses()
