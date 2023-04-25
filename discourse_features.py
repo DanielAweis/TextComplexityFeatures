2143c923e4c0bb7a6123d95e5ef1fc9984a00990
Daniela Weiss <daniela.weiss@mail.de>
2023-04-25 09:51:53 +0200

    wip: discourse features and discourse marker senses

--- discourse_features.py
+++ discourse_features.py
@@ -1,3 +1,4 @@
+import csv
 import spacy
 from itertools import repeat
 
@@ -7,6 +8,27 @@ from utils import get_data_from_json_file
 discourse_markers = get_data_from_json_file("discourse_markers.json")
 discourse_markers_with_sense = get_data_from_json_file("discourse_markers_with_sense.json")
 all_disc_marker_senses = get_data_from_json_file("all_discourse_marker_senses.json")
+
+disc_marker_senses_to_index = dict()
+for i, value in enumerate(all_disc_marker_senses):
+    feature = "".join(value)
+    # add these features after the features from the main script
+    disc_marker_senses_to_index[feature] = i
+
+# create a feature to index dict to keep track of order of elements
+features = [
+            # Cohesion features
+            "average_count_of_pronouns_per_sentence",
+            "average_count_of_definite_articles_per_sentence",
+            # coherence feature
+            "average_count_of_discourse_markers_per_sentence"
+            ]
+
+feature_to_index = dict()
+for i, value in enumerate(features):
+    feature = "".join(value)
+    # add these features after the features from the main script
+    feature_to_index[feature] = i + 27
 
 
 # Cohesion features
@@ -68,6 +90,26 @@ def get_count_for_discourse_marker_senses(doc):
     return counts_vec
 
 
+def calculate_all_features(doc):
+    return [
+        # Cohesion features
+        get_average_count_of_pronouns_per_sentence(doc),
+        get_average_count_of_definite_articles_per_sentence(doc),
+        # coherence feature
+        get_average_count_of_discourse_markers_per_sentence(doc)
+    ]
+
+
+def get_discourse_features_for_all_docs(list_of_dicts, nlp):
+    results = dict()
+    for elem in list_of_dicts:
+        doc = nlp(elem["text"])
+        # vec = get_count_for_discourse_marker_senses(doc)
+        vec = calculate_all_features(doc)
+        results[elem["id"]] = vec
+    return results
+
+
 def demo():
     nlp = spacy.load("de_core_news_sm")
     text = "Das ist meine tolle Banane. " \
@@ -95,5 +137,94 @@ def demo():
     # [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 
+def main():
+    # TODO: integrate this in main script!
+    miniklexi_file_path = "data/miniklexi_corpus.txt"
+    klexikon_file_path = "data/klexi_corpus.txt"
+    wiki_file_path = "data/wiki_corpus.txt"
+
+    json_miniklexi = get_data_from_json_file(miniklexi_file_path)
+    json_klexikon = get_data_from_json_file(klexikon_file_path)
+    json_wiki = get_data_from_json_file(wiki_file_path)
+
+    miniklexi = json_miniklexi["einfache"]  # label 0.0
+    klexikon = json_klexikon["klexikon"]  # label 0.5
+    wiki = json_wiki["wiki"]  # label 1.0
+
+    nlp = spacy.load("de_core_news_md")
+    result_small_miniklexi = get_discourse_features_for_all_docs(miniklexi, nlp)
+    result_small_klexikon = get_discourse_features_for_all_docs(klexikon, nlp)
+    result_small_wiki = get_discourse_features_for_all_docs(wiki, nlp)
+
+    # save results in one csv file
+    header = ['#id']
+    header_features = list(feature_to_index.keys())
+    header.extend(header_features)
+
+    with open("data/discourse_feature_vectors.csv", "w", encoding="utf-8") as csv_ofile:
+        writer = csv.writer(csv_ofile, delimiter=',')
+        writer.writerow(i for i in header)
+        for key, value in sorted(result_small_miniklexi.items()):
+            line = [f"miniklexi_{key}"]
+            vecs = [round(i, 6) for i in value]
+            line.extend(vecs)
+            writer.writerow(line)
+        for key, value in sorted(result_small_klexikon.items()):
+            line = [f"klexikon_{key}"]
+            vecs = [round(i, 6) for i in value]
+            line.extend(vecs)
+            writer.writerow(line)
+        for key, value in sorted(result_small_wiki.items()):
+            line = [f"wiki_{key}"]
+            vecs = [round(i, 6) for i in value]
+            line.extend(vecs)
+            writer.writerow(line)
+
+
+def discourse_marker_senses():
+    miniklexi_file_path = "data/miniklexi_corpus.txt"
+    klexikon_file_path = "data/klexi_corpus.txt"
+    wiki_file_path = "data/wiki_corpus.txt"
+
+    json_miniklexi = get_data_from_json_file(miniklexi_file_path)
+    json_klexikon = get_data_from_json_file(klexikon_file_path)
+    json_wiki = get_data_from_json_file(wiki_file_path)
+
+    miniklexi = json_miniklexi["einfache"]  # label 0.0
+    klexikon = json_klexikon["klexikon"]  # label 0.5
+    wiki = json_wiki["wiki"]  # label 1.0
+
+    nlp = spacy.load("de_core_news_md")
+    result_small_miniklexi = get_discourse_features_for_all_docs(miniklexi, nlp)
+    result_small_klexikon = get_discourse_features_for_all_docs(klexikon, nlp)
+    result_small_wiki = get_discourse_features_for_all_docs(wiki, nlp)
+
+    # save results in one csv file
+    header = ['#id']
+    header_features = list(disc_marker_senses_to_index.keys())
+    header.extend(header_features)
+
+    with open("data/discourse_marker_senses_vectors.csv", "w", encoding="utf-8") as csv_ofile:
+        writer = csv.writer(csv_ofile, delimiter=',')
+        writer.writerow(i for i in header)
+        for key, value in sorted(result_small_miniklexi.items()):
+            line = [f"miniklexi_{key}"]
+            vecs = [round(i, 6) for i in value]
+            line.extend(vecs)
+            writer.writerow(line)
+        for key, value in sorted(result_small_klexikon.items()):
+            line = [f"klexikon_{key}"]
+            vecs = [round(i, 6) for i in value]
+            line.extend(vecs)
+            writer.writerow(line)
+        for key, value in sorted(result_small_wiki.items()):
+            line = [f"wiki_{key}"]
+            vecs = [round(i, 6) for i in value]
+            line.extend(vecs)
+            writer.writerow(line)
+
+
 if __name__ == "__main__":
-    demo()
+    # demo()
+    # main()
+    discourse_marker_senses()
