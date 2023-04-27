87821c64586d60e5aa7bb1abd08b479dc43039dd
Daniela Weiss <daniela.weiss@mail.de>
2023-04-27 12:19:29 +0200

    wip

--- analyse_text_complexity.py
+++ analyse_text_complexity.py
@@ -2,7 +2,7 @@ from os import scandir
 from pathlib import Path
 import spacy
 from extract_features import calculate_all_features
-from utils_and_preprocess.utils import save_to_json_file
+from utils_and_preprocess.utils import save_to_json_file, check_input_validity
 
 
 def extract_features_for_all_docs(directory_path, nlp):
@@ -21,15 +21,19 @@ def extract_features_for_all_docs(directory_path, nlp):
             if document.name.endswith(".txt"):
                 temp_inputfile = Path(directory_path + document.name)
                 text = temp_inputfile.read_text(encoding="utf-8")
-                doc = nlp(text)
-                feature_vector = calculate_all_features(doc, nlp)
-                results[document.name.strip(".txt")] = feature_vector
+                try:
+                    check_input_validity(document.name, text, nlp)
+                    doc = nlp(text)
+                    feature_vector = calculate_all_features(doc, nlp)
+                    results[document.name.strip(".txt")] = feature_vector
+                except:
+                    continue
 
     return results
 
 
 def main():
-    summaries_dir_path = "data/model_summaries/"
+    summaries_dir_path = "data/small_sum/"
     nlp = spacy.load("de_core_news_md")
     text_complexity_features = extract_features_for_all_docs(summaries_dir_path, nlp)
     save_to_json_file(text_complexity_features, "text_complexity_feature_vectors.json")
